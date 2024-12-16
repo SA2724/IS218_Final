e9bdfb21e4abcb4b6d71616b7a40568b76fa972e
SA2724 <sa2724@njit.edu>
2024-12-15 22:54:42 -0500

    changed operations folder

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -3,19 +3,6 @@
 """
 Module: operations.py
 
-This module contains basic arithmetic functions that perform addition, subtraction,
-multiplication, and division of two numbers. These functions are foundational for
-building more complex applications, such as calculators or financial tools.
-
-Functions:
-- add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]: Returns the sum of a and b.
-- subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]: Returns the difference when b is subtracted from a.
-- multiply(a: Union[int, float], b: Union[int, float]) -> Union[int, float]: Returns the product of a and b.
-- divide(a: Union[int, float], b: Union[int, float]) -> float: Returns the quotient when a is divided by b. Raises ValueError if b is zero.
-
-Usage:
-These functions can be imported and used in other modules or integrated into APIs
-to perform arithmetic operations based on user input.
 """
 
 from typing import Union  # Import Union for type hinting multiple possible types
